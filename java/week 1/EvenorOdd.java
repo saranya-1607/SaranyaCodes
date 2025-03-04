68dedcc7fa190f612ee750e44553825f7deaa65f
Saranya A <asaranya131@gmail.com>
2025-03-04 20:16:53 +0530

    Create EvenorOdd.java

---
+++ java/week 1/EvenorOdd.java
@@ -0,0 +1,10 @@
+class Main{
+  public static void main(String[] args){
+    int n=8;
+    if(n%2==0){
+        System.out.println("n is even");
+    }else{
+        System.out.println("n is Odd");
+    }
+  }
+}
