69720b8d6798ba415ef3b1afa2dc1a99d3145fbb
Saranya A <asaranya131@gmail.com>
2025-03-04 18:47:28 +0530

    Create pattern.10.java
    OUTPUt:
    123456
    23456
    3456
    456
    56
    6

---
+++ pattern.10.java
@@ -0,0 +1,13 @@
+
+class Main {
+    public static void main(String[] args) {
+      int n=6;
+      
+      for(int i=1;i<=n;i++){
+          for(int j=i;j<=n;j++){
+              System.out.print(j);
+          }
+          System.out.println();
+      }
+    }
+}
