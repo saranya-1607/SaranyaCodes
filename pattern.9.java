6619467a54857b98edca21b5506b24129bb0a27a
Saranya A <asaranya131@gmail.com>
2025-03-04 18:42:56 +0530

    Update pattern.9.java
      output:       
              * 
            * * * 
          * * * * * 
        * * * * * * * 
      * * * * * * * * * 
    * * * * * * * * * * * 
      * * * * * * * * * 
        * * * * * * * 
          * * * * * 
            * * * 
              *

--- pattern.9.java
+++ pattern.9.java
@@ -1,7 +1,7 @@
 class Main {
     public static void main(String[] args) {
       int n=6;
-      for(int i=1;i<=n;i++){
+      for(int i=1;i<=n-1;i++){
           for(int j=1;j<=n-i;j++){
               System.out.print("  ");
           }
