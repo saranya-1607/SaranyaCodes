7e6b038bdd856298077c3d87a1b3e10038692f11
Saranya A <asaranya131@gmail.com>
2025-03-11 10:28:39 +0530

    Create  Nthtermseries.java

---
+++ java/week 1/Nthtermseries.java
@@ -0,0 +1,10 @@
+class  Nthtermseries {
+    public static void main(String[] args) {
+        int n =6;
+        int sum=0;
+        for(int i=1;i<=n;i++){
+            sum =sum+i;
+        }
+        System.out.println(sum);
+    }
+}
