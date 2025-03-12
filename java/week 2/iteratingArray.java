c20366b66f872541a78d54e426e801ed606eb786
Saranya A <asaranya131@gmail.com>
2025-03-12 22:44:43 +0530

    Create iteratingArray.java

---
+++ java/week 2/iteratingArray.java
@@ -0,0 +1,9 @@
+ class iteratingArray {
+    public static void main(String[] args) {
+       int [] arr = {1,2,3,4,5,78};
+       int n = arr.length;
+       for(int i=0;i<=n-1;i++){
+           System.out.print(arr[i]+" ");
+       }
+    }
+}
