d430acf33b42f85f6e04eab99c81c4a937a88386
Saranya A <asaranya131@gmail.com>
2025-03-12 21:45:15 +0530

    Create Swap.java

---
+++ java/week 1/Swap.java
@@ -0,0 +1,10 @@
+class Swap {
+    public static void main(String[] args) {
+        int a =19,b=12;
+        System.out.println("before swapping:a="+a+" b="+b);
+        int temp =a;
+        a=b;
+        b=temp;
+        System.out.println("after swapping:a="+a+" b="+b);
+    }
+}
