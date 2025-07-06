359f8fb6a7f59e074bd3f5bf4217d48f98a00b37
Saranya A <asaranya131@gmail.com>
2025-07-06 22:14:55 +0530

    fibonacci.java

--- java/week 3-basic recursion/fibonacci.java
+++ java/week 3-basic recursion/fibonacci.java
@@ -1,12 +1,11 @@
 class fibonacci{
-    public static void main(String[] args){
+    public static long fib(int n){
+        if(n==0) return 0;
+        if(n==1) return 1;
+        return fib(n-1)+fib(n-2);
+    }
+     public static void main(String[] args){
         int n=7;
         System.out.println(fib(n));
-    }
-    public static long fib(int n){
-        if(n<=1){
-            return 1;
-        }
-        return fib(n-1)+fib(n-2);
     }
 }
