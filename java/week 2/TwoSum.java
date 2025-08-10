0ecd0fb6d375739cfdbb3e379b18452ac77a691f
Saranya A <asaranya131@gmail.com>
2025-08-10 09:00:16 +0530

    Update TwoSum.java

--- java/week 2/TwoSum.java
+++ java/week 2/TwoSum.java
@@ -23,3 +23,26 @@ class TwoSum {
 
 // OPTIMISED HASHMAP SOLUTION 0(N)
 
+import java.util.*;
+class solution{
+    public int[] twosum(int[] nums,int target){
+        HashMap<Integer,Integer> map = new HashMap<>();
+        for(int i=0;i<nums.length;i++){
+            int compliment = target - nums[i];
+            if(map.containsKey(compliment)){
+                return new int[] {map.get(compliment),i};
+            }
+             map.put(nums[i],i);
+        }
+        return new int[] {};
+    }
+    public static void main(String[] args){
+        int[] nums ={1,3,6,7,8};
+        int target = 13;
+        solution obj = new solution();
+        int[] result = obj.twosum(nums,target);
+        System.out.println(Arrays.toString(result));
+        
+    }
+}
+
