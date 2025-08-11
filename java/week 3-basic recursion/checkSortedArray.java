36264897892035d03dcfa6ca82bcb41006f7b70a
Saranya A <asaranya131@gmail.com>
2025-08-11 20:26:16 +0530

    Create checkSortedArray.java

---
+++ java/week 3-basic recursion/checkSortedArray.java
@@ -0,0 +1,16 @@
+class checkSortedArray{
+    public static boolean checksort(int[] arr,int n ,int i){
+        if(i>=n-1){
+            return true;
+        }
+        if(arr[i]>arr[i+1]){
+            return false;
+        }
+        return checksort(arr,n,i+1);
+    }
+    public static void main(String[] args){
+        int[] arr ={1,2,3,4,5,1};
+        int n = arr.length;
+        System.out.println(checksort(arr,6,0));
+    }
+}
