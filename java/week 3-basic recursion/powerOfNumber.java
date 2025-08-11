d9651ace029937664b79cc0702b294591f220e88
Saranya A <asaranya131@gmail.com>
2025-08-11 21:03:14 +0530

    Create powerOfNumber.java

---
+++ java/week 3-basic recursion/powerOfNumber.java
@@ -0,0 +1,12 @@
+class powerOfNumber{
+    public static int power(int a,int b){
+        if(b==0){
+            return 1;
+        }
+        return a*power(a,b-1);
+    }
+    public static void main(String[] args){
+        int a=2,b=3;
+        System.out.println(power(a,b));
+    }
+}
