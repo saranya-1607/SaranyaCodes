496d11ceee7042c99266151f15993469006530a6
Saranya A <asaranya131@gmail.com>
2025-08-12 11:41:37 +0530

    Update BubbleSort.java

--- java/Sorting-I/BubbleSort.java
+++ java/Sorting-I/BubbleSort.java
@@ -1,3 +1,4 @@
+//WORST ,AVERAGE time complexity =O(N^2),
 import java.util.Arrays;
 class BubbleSort{
     public static void bubbleSort(int[] arr){
@@ -9,6 +10,34 @@ class BubbleSort{
                     arr[j+1] = arr[j];
                     arr[j] = temp;
                 }
+            }
+        }
+    }
+    public static void main(String[] args){
+        int[] arr={5,6,7,2,3,4,98,5,2};
+        bubbleSort(arr);
+        System.out.println(Arrays.toString(arr));
+    }
+}
+so WE OPTIMIZE THE CODE.BECAUSE WHAT IF ARRAY IS ALREAFY IN SORTED ORDER.NO NEED OF SWAPS RIGHT!? YEAH SO WE HAVE OPTIMIZED APPROACH WHICH HAVE 
+    TIME COMPLEXITY OF BEST CASE : O(N);
+code:
+import java.util.Arrays;
+class BubbleSort{
+    public static void bubbleSort(int[] arr){
+        int n = arr.length;
+        for(int i=n-1;i>=1;i--){
+            int didSwap =0;
+            for(int j=0;j<=i-1;j++){
+                if(arr[j] > arr[j+1]){
+                    int temp = arr[j+1];
+                    arr[j+1] = arr[j];
+                    arr[j] = temp;
+                    didSwap =1;
+                }
+            }
+            if(didSwap==0){
+                break;
             }
         }
     }
