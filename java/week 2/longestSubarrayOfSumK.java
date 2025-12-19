8282bb66ae8edf83d76c5a19df7e83ea2f73c259
Saranya A <asaranya131@gmail.com>
2025-12-19 18:54:44 +0530

    Update longestSubarrayOfSumK.java

--- java/week 2/longestSubarrayOfSumK.java
+++ java/week 2/longestSubarrayOfSumK.java
@@ -1,3 +1,29 @@
+//BRUTE FORCE APPROACH:
+class longestSubArray{
+    public static int longestSubarray(int[] nums,int k){
+        int res =0;
+        for(int i=0;i<nums.length;i++){
+            int sum =0;
+            for(int j=i;j<nums.length;j++){
+                sum += nums[j];
+            
+            if(sum == k){
+                int subLen = j-i +1;
+                res = Math.max(res,subLen);
+            }
+            }
+        }
+        return res;
+    }
+    public static void main(String[] args){
+        int[] nums ={1,2,3,4,1,1,1,1,2,3};
+        int k =4;
+        System.out.println(longestSubarray(nums,k));
+    }
+}
+
+////OPTIMIZED APPROACHH;::::::::::::::::;;;
+
 import java.util.HashMap;
 class longestSubarrayOfSumK{
     public static int longestSubarray(int[] arr,int k){
