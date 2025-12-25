e1004e707e190fae9d8c8c5d23a1c45029d99777
Saranya A <asaranya131@gmail.com>
2025-12-25 09:59:38 +0530

    Update 118. Pascal's Triangle.java

--- java/week2/ArraysHard/118. Pascal's Triangle.java
+++ java/week2/ArraysHard/118. Pascal's Triangle.java
@@ -1,1 +1,26 @@
-
+import java.util.*;
+class Main {
+    public static List<List<Integer>> generate(int numRows) {
+        int n = numRows;
+        List<List<Integer>> ans = new ArrayList<>();
+        List<Integer> firstRow =  new ArrayList<>();
+        firstRow.add(1);
+        ans.add(firstRow);
+        
+        for(int i=1;i<n;i++){
+            List<Integer>tempRow = new ArrayList<>();
+            tempRow.add(1);
+            for(int j=1;j<i-1;j++){
+                int val = ans .get(i-1).get(j)+ans.get(i-1).get(j-1);
+                tempRow.add(val);
+            }
+            tempRow.add(1);
+            ans.add(tempRow);
+        }
+        return ans;
+    }
+    public static void main(String[] args) {
+        System.out.print(generate(6));
+       
+    }
+}
