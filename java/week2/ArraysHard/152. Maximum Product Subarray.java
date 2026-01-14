33c393dc805540980929bbbf092a561ea1000e27
Saranya A <asaranya131@gmail.com>
2026-01-14 19:09:28 +0530

    Update 152. Maximum Product Subarray.java

--- java/week2/ArraysHard/152. Maximum Product Subarray.java
+++ java/week2/ArraysHard/152. Maximum Product Subarray.java
@@ -16,3 +16,20 @@ class Solution {
         return product;
     }
 }
+
+//////////////////////////OPTIMAL SOLUTION//////////////////
+class Solution {
+    public int maxProduct(int[] nums) {
+        int pref =1;
+        int suff =1;
+        int maxProd =Integer.MIN_VALUE;
+        for(int i=0;i<nums.length;i++){
+            if(pref==0) pref = 1;
+            if(suff==0) suff = 1;
+            pref *= nums[i];
+            suff *= nums[nums.length-i-1];
+            maxProd =Math.max(maxProd,Math.max(pref,suff));
+        }
+        return maxProd;
+    }
+}
