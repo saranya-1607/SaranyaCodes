69587adc6d54667e06a2ac2cb48857f861625e30
Saranya A <asaranya131@gmail.com>
2026-01-27 21:24:37 +0530

    Create 74. Search a 2D Matrix.java

---
+++ java/Binary Search/74. Search a 2D Matrix.java
@@ -0,0 +1,24 @@
+class Solution {
+    public boolean searchMatrix(int[][] matrix, int target) {
+        int m = matrix.length;
+        int n = matrix[0].length;
+        int l=0;
+        int r = m*n-1;
+        while(l<=r){
+            int mid=l+(r-l)/2;
+            int row = mid/n;
+            int col = mid%n;
+
+            int val=matrix[row][col];
+            if(val==target){
+                return true;
+            }
+            if(val>target){
+                r=mid-1;
+            }else{
+                l=mid+1;
+            }
+        }
+        return false;
+    }
+}
