7410bce695a52ee3eba88c7d41087c86546a5d1a
Saranya A <asaranya131@gmail.com>
2026-04-10 20:32:45 +0530

    Add brute force solution for minimum distance problem

---
+++ java/week 2/3740. Minimum Distance Between Three Equal Elements I.java
@@ -0,0 +1,18 @@
+///bruteforce 
+class Solution {
+    public int minimumDistance(int[] nums) {
+        int n=nums.length;
+        int minDist =Integer.MAX_VALUE;
+        for(int i=0;i<n;i++){
+            for(int j=i+1;j<n;j++){
+                for(int k=j+1;k<n;k++){
+                    if(nums[i]==nums[j] && nums[j]==nums[k]){
+                            int dist =Math.abs(i-j)+Math.abs(j-k)+Math.abs(i-k);
+                            minDist = Math.min(minDist,dist);
+                    }
+                }
+            }
+        }
+        return minDist==Integer.MAX_VALUE ? -1 : minDist;
+    }
+}
