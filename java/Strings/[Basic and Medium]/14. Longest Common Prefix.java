81dd2b275d533d91a29685a43ccbc14a35533eff
Saranya A <asaranya131@gmail.com>
2026-05-11 11:27:13 +0530

    Optimize longestCommonPrefix method implementation
    Refactored longestCommonPrefix method to use sorting for efficiency.

--- java/Strings/[Basic and Medium]/14. Longest Common Prefix.java
+++ java/Strings/[Basic and Medium]/14. Longest Common Prefix.java
@@ -1,3 +1,24 @@
+//optimal: TC:O(N), SC:O(1)
+class Solution {
+    public String longestCommonPrefix(String[] strs) {
+        String ans ="";
+        Arrays.sort(strs);
+        String first = strs[0];
+        String last = strs[strs.length-1];
+        for(int i=0;i<first.length();i++){
+            if(first.charAt(i) != last.charAt(i)){
+                break;
+            }else{
+                ans += first.charAt(i);
+            }
+        }
+        return ans;
+    }
+}
+
+
+
+
 class Solution {
     public String longestCommonPrefix(String[] strs) {
         if(strs==null || strs.length==0) return "";
