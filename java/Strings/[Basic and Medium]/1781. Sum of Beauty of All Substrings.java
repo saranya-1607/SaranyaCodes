59e6c9d2b160fd612b9fd3e29183a63e496f01da
Saranya A <asaranya131@gmail.com>
2026-05-17 20:12:43 +0530

    Create 1781. Sum of Beauty of All Substrings.java

---
+++ java/Strings/[Basic and Medium]/1781. Sum of Beauty of All Substrings.java
@@ -0,0 +1,32 @@
+//TC:O(n^2), SC:O(n)
+class Solution {
+    public int getMinCount(int[] freq){
+        int minCount = Integer.MAX_VALUE;
+        for(int i=0;i<26;i++){
+            if( freq[i] != 0){
+                minCount = Math.min(minCount,freq[i]);
+            }
+        }
+        return minCount;
+    }
+    public int getMaxCount(int[] freq){
+        int maxCount =0;
+        for(int i=0;i<26;i++){
+            maxCount = Math.max(maxCount,freq[i]);
+        }
+        return maxCount;
+    }
+    public int beautySum(String s) {
+        int sum =0;
+        
+        for(int i=0;i<s.length();i++){
+            int[] freq = new int[26];//0
+            for(int j=i;j<s.length();j++){
+                freq[s.charAt(j)-'a']++;
+                int beauty = getMaxCount(freq) - getMinCount(freq);
+                sum += beauty;
+            }
+        }
+        return sum;
+    }
+}
