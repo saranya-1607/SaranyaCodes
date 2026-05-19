b18318bbedcdf5f63a9ef75064e7829e7d5d139c
Saranya A <asaranya131@gmail.com>
2026-05-19 13:00:11 +0530

    Create printLinkedList.java

---
+++ java/Singly LinkedList/printLinkedList.java
@@ -0,0 +1,34 @@
+
+class Node{
+    String data;
+    Node next;
+    Node(String data){
+        this.data=data;
+        this.next=null;
+    }
+}
+class Main {
+    public static void printLinkedList(Node head){
+        Node temp = head;
+        while(temp != null){
+            System.out.print(temp.data);
+            temp = temp.next;
+            if(temp != null){
+                System.out.print("->");
+            }
+        }
+    }
+    public static void main(String[] args) {
+       Node n1 = new Node("A");
+       Node n2 = new Node("B");
+       Node n3 = new Node("C");
+       Node n4 = new Node("D");
+       n1.next = n2;
+       n2.next = n3;
+       n3.next = n4;
+       n4.next = null;
+       
+       Node head = n1;
+       printLinkedList(head);
+    }
+}
