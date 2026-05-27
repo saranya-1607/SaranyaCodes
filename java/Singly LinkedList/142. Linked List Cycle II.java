10fd24ce2a81fd6f39b4ca94a637cef8f9eabe3c
Saranya A <asaranya131@gmail.com>
2026-05-27 21:53:46 +0530

    Implement cycle detection in a linked list

---
+++ java/Singly LinkedList/142. Linked List Cycle II.java
@@ -0,0 +1,37 @@
+//TC:o(n)
+//SC:O(1)
+/**
+ * Definition for singly-linked list.
+ * class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode(int x) {
+ *         val = x;
+ *         next = null;
+ *     }
+ * }
+ */
+public class Solution {
+    public ListNode detectCycle(ListNode head) {
+        ListNode slow = head;
+        ListNode fast = head;
+
+        while(slow != null && fast.next != null && fast.next.next != null){
+            slow = slow.next;
+            fast = fast.next.next;
+
+            if(slow == fast){
+                ListNode ptr1 = head;
+                ListNode ptr2 = slow;
+
+                while(ptr1 != ptr2){
+                    ptr1=ptr1.next;
+                    ptr2=ptr2.next;
+                }
+                 return ptr1;
+            }
+           
+        }
+        return null;
+    }
+}
